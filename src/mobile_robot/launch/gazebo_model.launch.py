4034f48446ed0f90f9cb49690c7ecf57404f5d16
Peder Aa. Hoff <peder.aa.hoff@gmail.com>
2026-07-24 13:24:03 +0000

    Lagde en launchfil

---
+++ src/mobile_robot/launch/gazebo_model.launch.py
@@ -0,0 +1,73 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_descripton():
+    robotXacroName='differential_drive_robot'
+    namePackage='mobile_robot'
+    modelFileRelativePath = 'model/robot.xacro'
+    pathModelfile = os.path.join(get_package_share_directory(namePackage), modelFileRelativePath)
+    robotDescription = xacro.process_file(pathModelfile).toxml()
+
+
+    gazebo_rosPackageLaunch = PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'))
+
+    gazeboLaunch = IncludeLaunchDescription(gazebo_rosPackageLaunch, launch_arguments={'gz_args': ['-r -v -v4 empty.sdf'], 'on_exit_shutdown': 'True'}.items())
+
+    # Gazebo node
+    spawnModelNodeGazebo = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', robotXacroName,
+            '-topic', 'robot_description',
+        ],
+        output='screen'
+    )
+
+    # Robot state publisher node
+    nodeRobotStatePublisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robotDescription,
+                     'use_sim_time': True}]
+    )
+
+    bridge_params = os.path.join(
+        get_package_share_directory(namePackage),
+        'parameters',
+        'bridge_parameters.yaml'
+    )
+
+    start_gazebo_ros_bridge_cmd = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros_args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ],
+        output='screen',
+    )
+
+
+
+    # Create launch description
+    LaunchDescriptionObject = LaunchDescription()
+
+    # Add gazebolaunch
+    LaunchDescriptionObject.add_action(gazeboLaunch)
+
+    # Add the two nodes
+    LaunchDescriptionObject.add_action(spawnModelNodeGazebo)
+    LaunchDescriptionObject.add_action(nodeRobotStatePublisher)
+    LaunchDescriptionObject.add_action(start_gazebo_ros_bridge_cmd)
+
+    return LaunchDescriptionObject
+
